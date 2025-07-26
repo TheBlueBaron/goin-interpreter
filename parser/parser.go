9b47d563079480d5f09fefb69efa8a539461965d
TheBlueBaron <ronniehughjames@protonmail.com>
2025-07-26 17:02:14 +0100

    feat(): add return statement parsing, setup expression parsing

--- parser/parser.go
+++ parser/parser.go
@@ -8,12 +8,20 @@ import (
 )
 
 type Parser struct {
-	l 			*lexer.Lexer
-	errors		[]string
+	l 				*lexer.Lexer
+	errors			[]string
 
-	curToken 	token.Token
-	peekToken	token.Token
+	curToken 		token.Token
+	peekToken		token.Token
+
+	prefixParseFns	map[token.TokenType]prefixParseFn
+	infixParseFns 	map[token.TokenType]infixParseFn
 }
+
+type (
+	prefixParseFn	func() ast.Expression
+	infixParseFn 	func(ast.Expression) ast.Expression
+)
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{
@@ -51,6 +59,8 @@ func (p *Parser) parseStatement() ast.Statement {
     switch p.curToken.Type {
     case token.LET:
         return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
     default:
         return nil
     }
@@ -70,6 +80,19 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// TODO: Skip expressions until semicolon encountered
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: Skip expressions unitl semicolon encountered
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
@@ -102,4 +125,12 @@ func (p *Parser) Errors() []string {
 func (p *Parser) peekError(t token.TokenType) {
 	msg := fmt.Sprintf("expected next token to be %s, got %s instead.", t, p.peekToken.Type)
 	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
