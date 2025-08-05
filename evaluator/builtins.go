289e1cd922c42b364f652c7972fad5cf16609152
TheBlueBaron <ronniehughjames@protonmail.com>
2025-08-05 20:16:52 +0100

    chore(): add print function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"fmt"
 	"interpreter/object"
 )
 
@@ -88,5 +89,12 @@ var builtins = map[string]*object.Builtin {
 		newElements[length] = args[1]
 
 		return &object.Array{Elements: newElements}
+	},},
+	"puts": &object.Builtin{Fn: func(args ...object.Object) object.Object {
+		for _, arg := range args {
+			fmt.Println(arg.Inspect())
+		}
+
+		return &object.String{Value: "---------- ---------- ---------- ---------- ---------- ----------"}
 	},},
 }
